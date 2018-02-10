a4c7206b4bafb37923126bd0abd53687393e5123
Antonio Guerrero <jaguerrero@correo.uaa.mx>
2018-02-10 03:33:13 +0000

    version 2.0.2

---
+++ R/phasestats.R
@@ -0,0 +1,17 @@
+phasestats<-function(data,class,f) {
+    if (missing(data)) 
+        stop("'data' argument is not specified")
+    if (!is.emg(data)) 
+        stop("an object of class 'emg' is required")
+	ds<-rle(class)
+	res=rep(0,length(ds$lengths))
+        i<-1
+	for(j in 1:length(ds$lengths)){
+		res[j]<-f(data$values[i:(i+(ds$lengths[j]-1))])
+	i<-i+ds$lengths[j]
+	}
+    names(res)<-ds$values
+    object <- list(statistic=deparse(substitute(f)),stats=res)
+    class(object) <- "pstat"
+    return(object)
+}
